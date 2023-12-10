6d67b1b4546f11943be391ee2078bae1e5629890
Smith3G <mypom87@gmail.com>
2023-12-10 11:47:07 +0300

    add masters and shop katalogs

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ let link = document.querySelector(".menu_link1");
 
 
 let review = document.getElementById("review");
+let revews = document.querySelector(".reviews-wrap");
 
 
 function handleButtonClick() {
   review.scrollIntoView({behavior: "smooth" });
+
 }
+
+
 
 link.addEventListener("click", handleButtonClick);
